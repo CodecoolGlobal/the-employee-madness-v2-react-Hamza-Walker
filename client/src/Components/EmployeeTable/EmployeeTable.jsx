a9aa565a1a5172a4a68a3adf5fdb15a5c61a4328
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-05-18 07:14:05 +0200

    i filtered the already sorted employees and now task four is done i would say the solution has the desired effect, one rcomendation would be to go over the server side sorting and improve the code but for now its god enough

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -1,30 +1,50 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./EmployeeTable.css";
 
 const EmployeeTable = ({ employees, onDelete }) => {
-      const [sortOrder, setSortOrder] = useState("asc");
-  
-      const sortEmployees = (employees) => {
-        if (sortOrder === "asc") {
-          return employees.sort((a, b) => a.level.order - b.level.order);
-        } else {
-          return employees.sort((a, b) => b.level.order - a.level.order);
-        }
-      };
-      
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [filterLevel, setFilterLevel] = useState("");
 
+  const handleFilter = (e) => {
+    setFilterLevel(e.target.value);
+  };
+
+  const filterEmployees = (employees) => {
+    return employees.filter((employee) => {
+      return employee.level.level.toLowerCase().includes(filterLevel.toLowerCase());
+    });
+  };
+
+  const sortEmployees = (employees) => {
+    if (sortOrder === "asc") {
+      return employees.sort((a, b) => a.level.order - b.level.order);
+    } else {
+      return employees.sort((a, b) => b.level.order - a.level.order);
+    }
+  };
+
+  const filteredAndSortedEmployees = sortEmployees(filterEmployees(employees));
 
   return (
     <div className="EmployeeTable">
+      <div>
+        <input
+          type="text"
+          placeholder="Filter by level"
+          value={filterLevel}
+          onChange={handleFilter}
+        />
+      </div>
       <table>
         <thead>
           <tr>
             <th>First Name</th>
             <th>Last Name</th>
             <th>Position</th>
-            <th>Level
-            <button onClick={() => setSortOrder(sortOrder === "asc" ? "desc" : "asc")}>
+            <th>
+              Level
+              <button onClick={() => setSortOrder(sortOrder === "asc" ? "desc" : "asc")}>
                 {sortOrder === "asc" ? "▲" : "▼"}
               </button>
             </th>
@@ -32,14 +52,14 @@ const EmployeeTable = ({ employees, onDelete }) => {
           </tr>
         </thead>
         <tbody>
-          {sortEmployees(employees).map((employee) => (
+          {filteredAndSortedEmployees.map((employee) => (
             <tr key={employee._id}>
               <td>{employee.firstName}</td>
               <td>{employee.lastName}</td>
               <td>{employee.position}</td>
-               <td>{employee.level.level}</td>  {/* <== Update this line */}
+              <td>{employee.level.level}</td>
               <td>
-              <Link to={`/update/${employee._id}`}>
+                <Link to={`/update/${employee._id}`}>
                   <button type="button">Update</button>
                 </Link>
                 <button type="button" onClick={() => onDelete(employee._id)}>
