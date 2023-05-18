dd23c9ec717ac590fbf43557a2bc4af6e91910b1
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-05-18 07:04:02 +0200

    i actually had solved the first two task simountainiously and now im done with the third

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 import "./EmployeeTable.css";
 
 const EmployeeTable = ({ employees, onDelete }) => {
+      const [sortOrder, setSortOrder] = useState("asc");
+  
+      const sortEmployees = (employees) => {
+        if (sortOrder === "asc") {
+          return employees.sort((a, b) => a.level.order - b.level.order);
+        } else {
+          return employees.sort((a, b) => b.level.order - a.level.order);
+        }
+      };
+      
+
+
   return (
     <div className="EmployeeTable">
       <table>
@@ -11,17 +23,21 @@ const EmployeeTable = ({ employees, onDelete }) => {
             <th>First Name</th>
             <th>Last Name</th>
             <th>Position</th>
-            <th>Level</th>
+            <th>Level
+            <button onClick={() => setSortOrder(sortOrder === "asc" ? "desc" : "asc")}>
+                {sortOrder === "asc" ? "▲" : "▼"}
+              </button>
+            </th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {employees.map((employee) => (
+          {sortEmployees(employees).map((employee) => (
             <tr key={employee._id}>
               <td>{employee.firstName}</td>
               <td>{employee.lastName}</td>
               <td>{employee.position}</td>
-              <td>{employee.level}</td>
+               <td>{employee.level.level}</td>  {/* <== Update this line */}
               <td>
               <Link to={`/update/${employee._id}`}>
                   <button type="button">Update</button>
